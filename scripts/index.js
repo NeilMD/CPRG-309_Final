adaf44613d08a75fc0e2e74bf1d4bf5fcd3c5c58
neilmd <neil.capistrano2007@gmail.com>
2024-12-04 08:16:27 -0700

    Added some fixes

--- scripts/index.js
+++ scripts/index.js
@@ -3,8 +3,6 @@ window.addEventListener('scroll', function(){
     msg.style.transform = `translateY(-${window.scrollY/3}px)`;
 });
 
-
-
 document.getElementById('prev-btn').addEventListener('click', function() {
     const container = document.getElementById('discover-container');
     container.scrollLeft -= 250; // Scroll 100px to the left
@@ -14,3 +12,21 @@ document.getElementById('next-btn').addEventListener('click', function() {
     const container = document.getElementById('discover-container');
     container.scrollLeft += 250; // Scroll 100px to the right
 });
+
+
+// ANIMATION for Focus Section
+const focus = document.querySelectorAll('.from-left, .from-right');
+const slideObserver = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    
+      if(entry.target.classList.contains('from-left')) {
+        entry.target.classList.toggle("left-animate", entry.isIntersecting);
+      }else{
+        entry.target.classList.toggle("right-animate", entry.isIntersecting);
+      }
+  });
+}, {
+  threshold: 0.2
+});
+
+focus.forEach(im => slideObserver.observe(im));
