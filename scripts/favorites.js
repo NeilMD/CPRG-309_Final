c7679c1102e9aaebfe8b78adf216f614ce530d92
neilmd <neil.capistrano2007@gmail.com>
2024-12-08 11:10:49 -0700

    Added behavior for remove

--- scripts/favorites.js
+++ scripts/favorites.js
@@ -1,9 +1,10 @@
+let favorites = JSON.parse(localStorage.getItem(storageName));
 
-
-
-const fillFavorites = async()=>{
+const fillData = async()=>{
     let elTemp = await getTemplate('./templates/favorite-card.html');
-    const favorites = JSON.parse(localStorage.getItem(storageName));
+    
+
+    setTotalTime();
 
     for (let fav of favorites) {
         let el = elTemp.cloneNode(true);
@@ -15,7 +16,42 @@ const fillFavorites = async()=>{
 
         document.getElementById('music-wrapper').insertAdjacentElement('beforeend',el);
     }
-   
+    addBehavior();
 }
 
-fillFavorites();
+const setTotalTime = ()=>{
+    let totalDuration = 0;
+    favorites.forEach(element => {
+        const durationParts = element.duration.split(':');
+        const result = durationParts.length === 1 && durationParts[0] === "" ? ['0', '0'] : durationParts;
+        totalDuration += (parseInt(result[0],10) * 60);
+        totalDuration += (parseInt(result[1],10));
+    });
+
+    const min = String(Math.floor(totalDuration / 60)).padStart(2,0);
+    const sec = String(totalDuration % 60).padStart(2,0);
+
+    year.innerText = new Date().getFullYear();
+    detail.innerText = `${favorites.length} songs | ${min} minutes ${sec} seconds`
+}
+
+const addBehavior = () => {
+
+    document.querySelectorAll('.music-card').forEach((el)=>{
+        let name = el.getElementsByClassName('music-name')[0].innerText;
+        let artist = el.getElementsByClassName('artist-name')[0].innerText;
+        el.addEventListener('click', ()=>{
+            let index = favorites.find(el => el.name === name && el.artist === artist );
+            favorites.splice(index, 1);
+            localStorage.setItem(storageName, JSON.stringify(favorites));
+            el.classList.add('slide-out');
+            setTimeout(()=>{
+                el.classList.add('hidden');
+                setTotalTime();
+            },500);
+        });
+
+    })
+}
+
+fillData();
